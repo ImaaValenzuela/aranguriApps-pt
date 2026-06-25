43cb9678dc7016b4b3c36344a734f8f335bd13b8
Imanol Valenzuela <imanolvalenzuela03@gmail.com>
2026-06-25 10:11:11 -0300

    chore(ci): configure ktlint, kover 0.9.8, android lint, and pr title semantic checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    alias(libs.plugins.kover)
+    alias(libs.plugins.ktlint)
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(libs.plugins.androidApplication) apply false
@@ -6,4 +8,41 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlinx.kover")
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        android.set(true)
+        ignoreFailures.set(false)
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+        }
+        filter {
+            exclude { element ->
+                element.file.path.contains("/generated/") || element.file.path.contains("/build/")
+            }
+        }
+    }
+}
+
+kover {
+    reports {
+        verify {
+            rule {
+                bound {
+                    minValue = 60
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+    kover(project(":shared"))
+    kover(project(":androidApp"))
 }
