84bebf205e03e0a603a0dd63a30752782b19b566
Imanol Valenzuela <imanolvalenzuela03@gmail.com>
2026-06-26 17:57:22 -0300

    style: reformat MainViewController for improved readability

--- shared/src/iosMain/kotlin/com/mitimiti/app/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/mitimiti/app/MainViewController.kt
@@ -6,17 +6,18 @@ import androidx.compose.ui.window.ComposeUIViewController
 import com.mitimiti.app.auth.IosGoogleSignInHelper
 
 @Suppress("FunctionName")
-fun MainViewController() = ComposeUIViewController {
-    val token by IosGoogleSignInHelper.pendingToken.collectAsState()
+fun MainViewController() =
+    ComposeUIViewController {
+        val token by IosGoogleSignInHelper.pendingToken.collectAsState()
 
-    App(
-        onGoogleSignInClick = {
-            // iOS: el botón de Google en LoginScreen llama a este callback.
-            // Desde acé tenés que invocar GIDSignIn.shared.signIn()
-            // y en el handler llamar a:
-            //   IosGoogleSignInHelper.setGoogleToken(idToken)
-        },
-        googleIdToken = token,
-        onGoogleTokenConsumed = { IosGoogleSignInHelper.clearToken() },
-    )
-}
+        App(
+            onGoogleSignInClick = {
+                // iOS: el botón de Google en LoginScreen llama a este callback.
+                // Desde acé tenés que invocar GIDSignIn.shared.signIn()
+                // y en el handler llamar a:
+                //   IosGoogleSignInHelper.setGoogleToken(idToken)
+            },
+            googleIdToken = token,
+            onGoogleTokenConsumed = { IosGoogleSignInHelper.clearToken() },
+        )
+    }
